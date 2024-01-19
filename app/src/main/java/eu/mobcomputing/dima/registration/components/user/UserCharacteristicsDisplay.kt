51716a751f81cc8ae5d617f834312dd81b31b592
Alžbeta Fekiačová <xfekiacova@stuba.sk>
2024-01-19 11:41:33 +0100

    delete unused imports

--- app/src/main/java/eu/mobcomputing/dima/registration/components/user/UserCharacteristicsDisplay.kt
+++ app/src/main/java/eu/mobcomputing/dima/registration/components/user/UserCharacteristicsDisplay.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -29,7 +28,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
