e867052f893b9b85eed2ea8f6ca403cffa9a6756
Alžbeta Fekiačová <xfekiacova@stuba.sk>
2024-02-03 20:13:26 +0100

    add: login view testing

---
+++ app/src/test/java/eu/mobcomputing/dima/registration/viewmodels/LogInViewModelTest.kt
@@ -0,0 +1,47 @@
+package eu.mobcomputing.dima.registration.viewmodels
+
+import androidx.navigation.NavController
+import com.google.common.truth.Truth
+import com.google.firebase.auth.FirebaseAuth
+import eu.mobcomputing.dima.registration.uiEvents.LogInUIEvent
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.spy
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+class LogInViewModelTest{
+    @Mock
+    lateinit var firebaseAuth: FirebaseAuth
+
+    @Mock
+    lateinit var navController: NavController
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+    }
+
+    @Test
+    fun onEvent_EmailChanged_UpdateStateCorrectly() {
+        val viewModel = LogInViewModel()
+        val email = "john.smith@email.com"
+
+        viewModel.onEvent(LogInUIEvent.EmailChanged(email), navController)
+
+        Truth.assertThat(email).isEqualTo(viewModel.logInUIState.value.email)
+    }
+
+    @Test
+    fun onEvent_PasswordChanged_UpdateStateCorrectly() {
+        val viewModel = LogInViewModel()
+        val password = "Password123"
+
+        viewModel.onEvent(LogInUIEvent.PasswordChanged(password), navController)
+
+        Truth.assertThat(password).isEqualTo(viewModel.logInUIState.value.password)
+    }
+
+}
